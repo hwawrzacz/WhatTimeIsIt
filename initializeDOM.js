c2f121bd99a2977b5c1bc0c78d73f67a37a95fbd
Hubert <hwawrzacz.dev@gmail.com>
2020-05-17 18:51:44 +0200

    Remomove images, create css-based clock-face

---
+++ initializeDOM.js
@@ -0,0 +1,6 @@
+const colckFace = document.querySelector('.indices');
+
+for (let index = 0; index < 60; index++) {
+    const liElement = document.createElement('li');
+    colckFace.appendChild(liElement);
+}
